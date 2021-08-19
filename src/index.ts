d5b44d4ceff6f737a5823e305d7ca79f83c14700
nerrewton <nerrechiboy@gmail.com>
2021-08-19 18:43:55 -0500

    something

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 const app = express.application;
 
 app.get("/test", (request, response) => {
-  console.log("Hello world");
+  console.log("Hello world !!");
 });
 
 app.listen(8080, () => console.log("server running in port:8080"));

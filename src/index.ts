a2c2c884d6e6ef65213c280fb0bad922d8d4f7e1
nerrewton <nerrechiboy@gmail.com>
2021-08-19 18:31:22 -0500

    second commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+
+const app = express.application;
+
+app.get("/test", (request, response) => {
+  console.log("Hello world");
+});
+
+app.listen(8080, () => console.log("server running in port:8080"));
